7dc9035f3f9f4d7a271de693fe9726caf5137d71
chengzhitong <chengzhitong@vjwealth.com>
2017-01-20 15:27:49 +0800

    cec

--- CTCoreCategoryDemo/CTCoreCategoryDemo/ViewController.m
+++ CTCoreCategoryDemo/CTCoreCategoryDemo/ViewController.m
@@ -7,21 +7,84 @@
 //
 
 #import "ViewController.h"
+#import <NSMutableAttributedString+Attributes.h>
+#import <TTTAttributedLabel.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *lab;
+@property (nonatomic, strong)TTTAttributedLabel * attlab;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString * str = [NSString stringWithFormat:@"This is an example by @www.apple.com/"];
+    
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:str]; // assume string exists
+    NSRange urlRange = [str rangeOfString:@"@www.apple.com/"];
+    
+    self.attlab = [[TTTAttributedLabel alloc] initWithFrame:CGRectMake(20, 20, 210, 300)];
+    _attlab.attributedText = string;
+    _attlab.lineBreakMode = NSLineBreakByCharWrapping;
+    _attlab.numberOfLines = 0;
+    _attlab.delegate = self;
+    _attlab.linkAttributes = @{kTTTBackgroundFillColorAttributeName:[UIColor whiteColor]};
+    _attlab.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.attlab];
+
+    [_attlab addLinkToURL:[NSURL URLWithString:@"http://www.baidu.com"] withRange:urlRange];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)attributedLabel:(TTTAttributedLabel *)label
+   didSelectLinkWithURL:(NSURL *)url
+{
+    NSLog(@"url = %@", url.absoluteString);
+}
+
+- (void)addAttributedStringTextView
+{
+    NSString * str = [NSString stringWithFormat:@"This is an example by @www.apple.com/"];
+    
+    NSMutableAttributedString *string = [[NSMutableAttributedString alloc] initWithString:str]; // assume string exists
+    NSRange urlRange = [str rangeOfString:@"www.apple.com/"];
+    [string addAttribute:NSLinkAttributeName
+                   value:@"username://@www.apple.com/"
+                   range:urlRange];
+    [string addAttribute:NSForegroundColorAttributeName
+                   value:[UIColor blueColor]
+                   range:urlRange];
+    [string addAttribute:NSUnderlineStyleAttributeName
+                   value:@(NSUnderlineStyleNone)
+                   range:urlRange];
+    [string endEditing];
+    
+    UITextView * textView = [[UITextView alloc] initWithFrame:CGRectMake(100, 200, 200, 200)];
+    textView.backgroundColor =[UIColor whiteColor];
+    textView.delegate = self;
+    [textView setSelectable: YES];
+    [textView setEditable:NO];
+    textView.attributedText = string;
+    textView.dataDetectorTypes = UIDataDetectorTypeLink;
+    
+    [self.view addSubview:textView];
+}
+
+- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange
+{
+    NSLog(@"url :%@",URL);
+    if ([[URL scheme] isEqualToString:@"username"])
+    {
+        NSString *username = [URL host];
+        NSLog(@"username :%@",username);
+        return NO;
+    }
+    return YES;
 }
 
 @end
